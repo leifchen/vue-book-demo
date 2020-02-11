93d55fe8adbecd8546f0766b5848ac704c25566b
LeifChen <leifchen90@gmail.com>
2020-02-11 18:36:30 +0800

    feat: 新增用户登录API

--- node/app.js
+++ node/app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = require('./router')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use('/', router)
 
 app.get('/', (req, res) => {
--- node/router/user.js
+++ node/router/user.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const Result = require('../models/Result')
+const { login } = require('../services/user')
+const { md5 } = require('../utils')
+const { PWD_SALT } = require('../utils/constant')
 
 const router = express.Router()
 
 router.get('/info', (req, res, next) => {
   res.json('user info...')
+})
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body
+
+  password = md5(`${password}${PWD_SALT}`)
+  login(username, password).then(user => {
+    if (!user || user.length === 0) {
+      new Result('登录失败').fail(res)
+    } else {
+      new Result('登录成功').success(res)
+    }
+  })
 })
 
 module.exports = router
