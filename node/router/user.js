59788ac4ff8e38acf55feb373d72eae81264f6c7
LeifChen <leifchen90@gmail.com>
2020-02-12 13:31:10 +0800

    feat: 新增JWT认证

--- node/router/user.js
+++ node/router/user.js
@@ -1,26 +1,59 @@
 const express = require('express')
 const Result = require('../models/Result')
-const { login } = require('../services/user')
-const { md5 } = require('../utils')
+const { login, findUser } = require('../services/user')
+const { md5, decode } = require('../utils')
 const { PWD_SALT } = require('../utils/constant')
+const { body, validationResult } = require('express-validator')
+const boom = require('boom')
+const jwt = require('jsonwebtoken')
+const { PRIVATE_KEY, JWT_EXPIRED } = require('../utils/constant')
 
 const router = express.Router()
 
-router.get('/info', (req, res, next) => {
-  res.json('user info...')
-})
-
-router.post('/login', (req, res) => {
-  let { username, password } = req.body
-
-  password = md5(`${password}${PWD_SALT}`)
-  login(username, password).then(user => {
-    if (!user || user.length === 0) {
-      new Result('登录失败').fail(res)
+router.post(
+  '/login',
+  [
+    body('username').isString().withMessage('username类型不正确'),
+    body('password').isString().withMessage('password类型不正确')
+  ],
+  (req, res, next) => {
+    const err = validationResult(req)
+    if (!err.isEmpty()) {
+      const [{ msg }] = err.errors
+      next(boom.badRequest(msg))
     } else {
-      new Result('登录成功').success(res)
+      let { username, password } = req.body
+      password = md5(`${password}${PWD_SALT}`)
+      login(username, password).then(user => {
+        if (!user || user.length === 0) {
+          new Result('登录失败').fail(res)
+        } else {
+          const token = jwt.sign(
+            { username },
+            PRIVATE_KEY,
+            { expiresIn: JWT_EXPIRED }
+          )
+          new Result({ token }, '登录成功').success(res)
+        }
+      })
     }
-  })
+  }
+)
+
+router.get('/info', (req, res) => {
+  const decoded = decode(req)
+  if (decoded && decoded.username) {
+    findUser(decoded.username).then(user => {
+      if (user) {
+        user.roles = [user.role]
+        new Result(user, '获取用户信息成功').success(res)
+      } else {
+        new Result('获取用户信息失败').fail(res)
+      }
+    })
+  } else {
+    new Result('用户信息解析失败').fail(res)
+  }
 })
 
 module.exports = router
--- node/utils/constant.js
+++ node/utils/constant.js
@@ -1,6 +1,9 @@
 module.exports = {
   CODE_ERROR: -1,
   CODE_SUCCESS: 0,
+  CODE_TOKEN_EXPIRED: -2,
   debug: true,
   PWD_SALT: 'LeifChen',
+  PRIVATE_KEY: 'vue_book_leifchen',
+  JWT_EXPIRED: 60 * 60, // token失效时间
 }
