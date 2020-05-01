c081bc107127b67fcabbf4268f9adf21115a2d0f
LeifChen <leifchen90@gmail.com>
2020-05-01 20:26:24 +0800

    feat: 新增上传电子书API

--- node/router/index.js
+++ node/router/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const boom = require('boom')
 const userRouter = require('./user')
+const bookRouter = require('./book')
 const jwtAuth = require('./jwt')
 const Result = require('../models/Result')
 
@@ -16,6 +17,8 @@ router.get('/', (req, res) => {
 
 // 通过 userRouter 来处理 /user 路由，对路由处理进行解耦
 router.use('/user', userRouter)
+
+router.use('/book', bookRouter)
 
 /**
  * 集中处理404请求的中间件
--- node/utils/constant.js
+++ node/utils/constant.js
@@ -1,3 +1,6 @@
+const { env } = require('./env')
+const UPLOAD_PATH = env === 'dev' ? 'D:/upload/admin-upload-ebook' : '/home/upload/admin-upload-ebook'
+
 module.exports = {
   CODE_ERROR: -1,
   CODE_SUCCESS: 0,
@@ -6,4 +9,5 @@ module.exports = {
   PWD_SALT: 'LeifChen',
   PRIVATE_KEY: 'vue_book_leifchen',
   JWT_EXPIRED: 60 * 60, // token失效时间
+  UPLOAD_PATH
 }
